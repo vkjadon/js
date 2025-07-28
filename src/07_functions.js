5dc336dc6253dc186c39e7d28fc1594a48d3b742
Vijay K Jadon <37266244+vkjadon@users.noreply.github.com>
2025-07-28 06:43:40 +0000

    Function and Loop

--- src/07_functions.js
+++ src/07_functions.js
@@ -3,44 +3,48 @@
 //     console.log(number1 + number2);
 // }
 
-function addTwoNumbers(number1, number2){
+// function addTwoNumbers(number1, number2){
 
     // let result = number1 + number2
     // return result
-    return number1 + number2
-}
+    // return number1 + number2
+// }
 
-const result = addTwoNumbers(3, 5)
+// number1 and number2 are the parameters of the function in function definition
+
+// const result = addTwoNumbers(3, 5)
+
+// 3 and 5 are the arguements in function call
 
 // console.log("Result: ", result);
 
 
-function loginUserMessage(username = "sam"){
-    if(!username){
-        console.log("PLease enter a username");
-        return
-    }
-    return `${username} just logged in`
-}
+// function loginUserMessage(username = "sam"){
+//     if(!username){
+//         console.log("PLease enter a username");
+//         return
+//     }
+//     return `${username} just logged in`
+// }
 
 // console.log(loginUserMessage("Vijay"))
 // console.log(loginUserMessage("Vijay"))
 
 
-function calculateCartPrice(val1, val2, ...num1){
-    return num1
-}
+// function calculateCartPrice(val1, val2, ...num1){
+//     return num1
+// }
 
 // console.log(calculateCartPrice(200, 400, 500, 2000))
 
-const userObject = {
-    username: "Vijay",
-    prices: 199
-}
+// const userObject = {
+//     username: "Vijay",
+//     prices: 199
+// }
 
-function handleObject(anyobject){
-    console.log(`Username is ${anyobject.username} and price is ${anyobject.price}`);
-}
+// function handleObject(anyobject){
+//     console.log(`Username is ${anyobject.username} and price is ${anyobject.price}`);
+// }
 
 // handleObject(userObject)
 
@@ -49,52 +53,49 @@ function handleObject(anyobject){
 //     price: 399
 // })
 
-const myNewArray = [200, 400, 100, 600]
+// const myNewArray = [200, 400, 100, 600]
 
-function returnSecondValue(getArray){
-    return getArray[1]
-}
+// function returnSecondValue(getArray){
+//     return getArray[1]
+// }
 
 // console.log(returnSecondValue(myNewArray));
 // console.log(returnSecondValue([200, 400, 500, 1000]));
-let a = 300
-if (true) {
-    let a = 10
-    const b = 20
-    // console.log("INNER: ", a);
+// let a = 300
+// if (true) {
+//     let a = 10
+//     const b = 20
+//     // console.log("INNER: ", a);
     
-}
-
-
+// }
 
 // console.log(a);
 // console.log(b);
 // console.log(c);
 
+// function one(){
+//     const username = "Vijay"
 
-function one(){
-    const username = "Vijay"
-
-    function two(){
-        const website = "youtube"
+//     function two(){
+//         const website = "youtube"
         // console.log(username);
-    }
+    // }
     // console.log(website);
 
-     two()
+    //  two()
 
-}
+// }
 
 // one()
 
-if (true) {
-    const username = "Vijay"
-    if (username === "Vijay") {
-        const website = " youtube"
+// if (true) {
+//     const username = "Vijay"
+//     if (username === "Vijay") {
+//         const website = " youtube"
         // console.log(username + website);
-    }
+    // }
     // console.log(website);
-}
+// }
 
 // console.log(username);
 
@@ -104,28 +105,28 @@ if (true) {
 
 // console.log(addone(5))
 
-function addone(num){
-    return num + 1
-}
+// function addone(num){
+//     return num + 1
+// }
 
 
 
 //addThree(5)
 
-const addThree = function(num){
-    return num + 3
-}
+// const addThree = function(num){
+//     return num + 3
+// }
 
-const user = {
-    username: "Vijay",
-    price: 999,
+// const user = {
+//     username: "Vijay",
+//     price: 999,
 
-    welcomeMessage: function() {
-        console.log(`${user.username} , welcome to website`);
-        console.log(this);
-    }
+//     welcomeMessage: function() {
+//         console.log(`${user.username} , welcome to website`);
+//         console.log(this);
+//     }
 
-}
+// }
 /*
 Use `this` inside object methods instead of hardcoding the object name.
 If the variable (object) name `user` changes, this line would break.
@@ -138,56 +139,62 @@ If the variable (object) name `user` changes, this line would break.
 
 // Run `console.log(this)` on browser -> the Global context is window()
 
-function arrow(){
-    console.log(this);
-}
-arrow()
 
-function arrow(){
-    let username = "Vijay"
-    console.log(this.username);
-}
-
-arrow()
-
-// const chai = function () {
-//     let username = "Vijay"
-//     console.log(this.username);
+// function fn_this() {
+//     // let username = "Vijay"
+//     console.log(this);
+//     // console.log(this.username);
+// }
+// const fn_this = function () {
+//     // let username = "Vijay"
+//     console.log(this);
+//     // console.log(this.username);
 // }
 
-// const chai =  () => {
+// const fn_this =  () => {
 //     let username = "Vijay"
 //     console.log(this);
 // }
 
+// fn_this() // Only returns empty object or scope {}
 
-// chai()
+// Basic Arrow Function : Remove function keyword
 
 // const addTwo = (num1, num2) => {
 //     return num1 + num2
 // }
 
-// const addTwo = (num1, num2) =>  num1 + num2
+// console.log(addTwo(3, 4))
 
-// const addTwo = (num1, num2) => ( num1 + num2 )
+// const addTwo = (num1, num2) =>  num1 + num2 
 
-// const addTwo = (num1, num2) => ({username: "Vijay"})
+//Implicit return : One liner without curly braces and retuen keyword
 
+// We can also use paranthesis for implicit return whereas {} are used for explicit return
+
+// const addTwo = (num1, num2) => (num1 + num2 )
+
+// To return an object in implecit return, we have to wrap the object in paranthesis ()
+
+// const addTwo = (num1, num2) => ({
+//     username: "Vijay",
+//     sum : (num1 + num2)
+// });
 
 // console.log(addTwo(3, 4))
 
 
-// const myArray = [2, 5, 3, 7, 8]
-
-// myArray.forEach()
-
 // Immediately Invoked Function Expressions (IIFE)
 
+(function ie(){
+    console.log(`DB CONNECTED`);
+})()
 
-// (function chai(){
-//     // named IIFE
-//     console.log(`DB CONNECTED`);
-// })();
+// IIFE with arrow function
+
+// ( () => {
+//     console.log(`DB CONNECTED TWO`);
+// } )();
 
 // ( (name) => {
 //     console.log(`DB CONNECTED TWO ${name}`);
