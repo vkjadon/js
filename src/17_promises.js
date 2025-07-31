e1f168745d885bb9ec9c2db104a0f980d7979ecf
Vijay K Jadon <37266244+vkjadon@users.noreply.github.com>
2025-07-31 16:47:13 +0000

    Promise

--- src/17_promises.js
+++ src/17_promises.js
@@ -1,105 +1,71 @@
-const promiseOne = new Promise(function(resolve, reject){
-    //Do an async task
-    // DB calls, cryptography, network
-    setTimeout(function(){
-        console.log('Async task is compelete');
-        resolve()
-    }, 1000)
-})
+// When we create a Promise, we don’t immediately have the value it will produce.
+// It acts as a placeholder (proxy) for a future result.
 
-promiseOne.then(function(){
-    console.log("Promise consumed");
-})
-
-new Promise(function(resolve, reject){
-    setTimeout(function(){
-        console.log("Async task 2");
-        resolve()
-    }, 1000)
-
-}).then(function(){
-    console.log("Async 2 resolved");
-})
-
-const promiseThree = new Promise(function(resolve, reject){
-    setTimeout(function(){
-        resolve({username: "Chai", email: "chai@example.com"})
-    }, 1000)
-})
-
-promiseThree.then(function(user){
-    console.log(user);
-})
-
-const promiseFour = new Promise(function(resolve, reject){
-    setTimeout(function(){
-        let error = true
-        if (!error) {
-            resolve({username: "hitesh", password: "123"})
-        } else {
-            reject('ERROR: Something went wrong')
-        }
-    }, 1000)
-})
-
- promiseFour
- .then((user) => {
-    console.log(user);
-    return user.username
-}).then((username) => {
-    console.log(username);
-}).catch(function(error){
-    console.log(error);
-}).finally(() => console.log("The promise is either resolved or rejected"))
-
-
-
-const promiseFive = new Promise(function(resolve, reject){
-    setTimeout(function(){
-        let error = true
-        if (!error) {
-            resolve({username: "javascript", password: "123"})
-        } else {
-            reject('ERROR: JS went wrong')
-        }
-    }, 1000)
+let promiseOne = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    resolve("Data loaded");
+  }, 2000); // This is scheduled to happen after 2 seconds.
 });
 
-async function consumePromiseFive(){
+// Here, promise is a proxy for the value "Data loaded"
+// we don't know it yet, but we will get it after 2 seconds.
+
+// We can associate a handlers to handle eventual success value or failure reason of an asynchronous action.
+// .then() and .catch() are the handlers for success and failure respectively.
+
+promiseOne
+  .then((data) => {     // data is the value we pass to the resolve function in Promise created
+    console.log("Success:", data);  // if resolved
+  })
+  .catch((error) => { // error is the value we pass to the reject function in Promise created
+    console.error("Error:", error);  // if rejected
+  });
+
+// Again to understand - 
+// Creating a new Promise requires an executor function. 
+// That function takes two parameters: resolve (for success) and reject (for failure). 
+// You call resolve() with a result when the operation completes successfully, 
+// We can call reject() with an error when it fails. 
+// This whole setup returns a Promise that can be handled using .then() and .catch().
+
+let promiseTwo = new Promise(function (resolve, reject) {
+  setTimeout(() => {
+    console.log("Inside setTimeout → trying to resolve...");
+    resolve("Data loaded in Promise Two");
+  }, 1000);
+
+  console.log("Rejecting immediately...");
+  reject("Error");
+});
+
+promiseTwo
+  .then((data) => console.log("Then:", data))
+  .catch((err) => console.log("Catch:", err));
+
+
+async function getAllUsers(){
     try {
-        const response = await promiseFive
-        console.log(response);
+        const response = await fetch('https://jsonplaceholder.typicode.com/users')
+
+        const data = await response.json()
+        console.log(data);
     } catch (error) {
-        console.log(error);
+        console.log("E: ", error);
     }
 }
 
-consumePromiseFive()
-
-// async function getAllUsers(){
-//     try {
-//         const response = await fetch('https://jsonplaceholder.typicode.com/users')
-
-//         const data = await response.json()
-//         console.log(data);
-//     } catch (error) {
-//         console.log("E: ", error);
-//     }
-// }
-
 //getAllUsers()
 
-fetch('https://api.github.com/users/hiteshchoudhary')
+fetch('https://api.github.com/users/vkjadon')
 .then((response) => {
     return response.json()
 })
 .then((data) => {
-    console.log(data);
+    // console.log(data);
+    console.log("data");
 })
 .catch((error) => console.log(error))
 /*
 fetch is not exactly equivalent to manually creating a promise using new Promise(...).
 However, fetch returns a Promise, which behaves the same as a manually created promise — just abstracted away.
 */
-// promise.all
-// yes this is also available, kuch reading aap b kro.
