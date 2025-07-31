2ed7e638f4b71c6d2dcb4089c3d94daa9d821133
Vijay K Jadon <37266244+vkjadon@users.noreply.github.com>
2025-07-31 08:14:22 +0000

    Updated Callback Function

--- src/15_callback.js
+++ src/15_callback.js
@@ -1,18 +1,52 @@
-// The consumer writes a function (the callback)
-// You (as the user or consumer of the API) define a function to be passed as an argument.
+// Method - 1
 
-function myCallback(result) {
-  console.log("Result is:", result);
-}
 
-// The API provider (the "caller") accepts that function and uses it
-// They invoke your function inside theic logic.
-// Following is the caller function 
+// Step -1: 
+// The API provider writes a caller function(API) which accepts a function called callback function
+// and invoke this callback function anywhere inside the API definition.
+
+// Following is the caller function API 
 
 function doMath(a, b, callback) {
   let sum = a + b;
-  callback(sum);  // calling back with the result
+  callback(sum);
+}
+
+// Step-2:
+// The consumer writes a function (the callback) and pass this function to API as an argument.
+
+function myCallback(result) {
+  result = result**2
+  console.log("Result is:", result);
 }
 
 // So here 'doMath' is an API which is defined to take two parameters and one function (callback)
-// Here 'myCallback'
+// Here 'myCallback' is the callback function, The number of parameters can be any. We can pass array or even objects.
+
+// We can call 'doMath' API as below
+
+// doMath(5, 3, myCallback);
+
+// The caller is responsible for passing the right parameters into the callback function.
+
+// Method - 2
+
+// API
+
+function checkNumber(num, callback) {
+  console.log(callback.toString())
+  let shouldProceed = callback(num);
+  if (shouldProceed) {
+    console.log("Proceeding with:", num);
+  } else {
+    console.log("Not allowed");
+  }
+}
+
+// Calling this API using Arrow Function
+
+checkNumber(10, (n) => n > 5);  // returns true → prints "Proceeding with: 10"
+checkNumber(2, (n) => n > 5);   // returns false → prints "Not allowed"
+
+
+
